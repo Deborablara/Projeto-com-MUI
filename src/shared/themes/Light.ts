8dca7de51cb64c447a4fc7484da39a1096fef9a3
Deborablara <debora.blara19@gmail.com>
2022-06-28 20:21:47 -0300

    chore: modificando tema claro

--- src/shared/themes/Light.ts
+++ src/shared/themes/Light.ts
@@ -4,9 +4,9 @@ import { cyan, yellow } from '@mui/material/colors';
 export const LightTheme = createTheme({
   palette: {
     primary: {
-      main: '#515151',
-      dark: '#3c3c3c',
-      light:'#8c8c8c',
+      main: yellow[700],
+      dark: yellow[800],
+      light: yellow[500],
       contrastText: '#ffffff',
 
     },
